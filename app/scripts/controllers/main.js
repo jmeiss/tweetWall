dc3eed26ba7706f917bb6d92d5586bf17b6b508b
Jonathan Meiss <jonathan@meiss.fr>
2013-06-24 00:05:29 +0200

    fix time ago

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -53,11 +53,30 @@ angular.module('tweetwallApp')
       return {show:'animated ' + ngAnimate[0], hide:'animated ' + ngAnimate[1]}
     }
 
+    var parseTwitterDate = function (tdate) {
+      var system_date = new Date(Date.parse(tdate));
+      var user_date = new Date();
+      var diff = Math.floor((user_date - system_date) / 1000);
+
+      if (diff <= 1) {return "just now";}
+      if (diff < 20) {return diff + " seconds ago";}
+      if (diff < 40) {return "half a minute ago";}
+      if (diff < 60) {return "less than a minute ago";}
+      if (diff <= 90) {return "one minute ago";}
+      if (diff <= 3540) {return Math.round(diff / 60) + " minutes ago";}
+      if (diff <= 5400) {return "1 hour ago";}
+      if (diff <= 86400) {return Math.round(diff / 3600) + " hours ago";}
+      if (diff <= 129600) {return "1 day ago";}
+      if (diff < 604800) {return Math.round(diff / 86400) + " days ago";}
+      if (diff <= 777600) {return "1 week ago";}
+      return "on " + system_date;
+    }
+
     var getTweet = function () {
       var tweetRandPos = Math.floor(Math.random() * nbTweetsToLoad)
 
       $scope.tweet = $scope.tweets[tweetRandPos]
-      $scope.tweetDate = moment($scope.tweetDate).fromNow()
+      $scope.tweetDate = parseTwitterDate($scope.tweet.created_at)
       $scope.newBackgroundColor = getNewBackgroundColor()
       $scope.nextAnimation = getNextAnimation()
       
